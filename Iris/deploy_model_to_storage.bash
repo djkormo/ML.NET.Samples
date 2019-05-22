c5a222ea2c47f4b82a2ce72ad49e9ced3340427c
Twoje imię i nazwsko <djkormo@gmail.com>
2019-05-23 00:02:25 +0200

    adding deploy_model_to_storage.bash script in Iris

---
+++ Iris/deploy_model_to_storage.bash
@@ -0,0 +1,36 @@
+RND=$RANDOM
+
+ML_LOCATION=northeurope
+ML_GROUP=rg-ml-net
+ML_STACCOUNT=staccmlnet$RND
+ML_FUNCAPP=mlnetfuncapp$RND
+
+# domyslna nazwa grupy 
+az configure --defaults group=$ML_GROUP
+
+# domyslna lokalizacja rejestru z obrazami 
+az configure --defaults location=$ML_LOCATION
+
+# odswiezamy zawartosc repozytorium w ramach galęzi master 
+git checkout master
+
+# pobieramy zawartość repozytorium 
+git pull
+
+# weryfikujemy niespójności zawartości lokalnego i zdalnego repozytorium 
+git status
+
+
+
+az group create --name $ML_GROUP --location $ML_LOCATION
+
+az storage account create --name $ML_STACCOUNT --location $ML_LOCATION --resource-group $ML_GROUP --sku Standard_LRS
+
+az functionapp create --name $ML_FUNCAPP --storage-account $ML_STACCOUNT --consumption-plan-location $ML_LOCATION --resource-group $ML_GROUP
+
+az functionapp config appsettings set --name $ML_FUNCAPP --resource-group $ML_GROUP --settings FUNCTIONS_EXTENSION_VERSION=beta
+
+az storage account keys list --account-name $ML_STACCOUNT --resource-group $ML_GROUP
+
+
+#az storage container create --name models --account-key YOURKEY --account-name mlnetdemostorage1
